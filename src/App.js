3a9aa8fe09758c993aeea8a54f38b3106a524ee3
chandu <chandukuruba110@gamail.com>
2023-10-13 08:45:37 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import Header from './components/Header'
-import BestInClass from './components/BestInClass'
-import Expading from './components/Expading'
-import TopPackages from './components/TopPackages'
-import WhyChooseUs from './components/WhyChoosUs'
-import BottomSection from './components/BottomSection'
+import QrScanner from './frontend/components/QrScanner'
+
 import './App.css'
 
 const App = () => (
-  <>
-    <Header />
-    <BestInClass />
-    <Expading />
-    <TopPackages />
-    <WhyChooseUs />
-    <BottomSection />
-  </>
+  <div>
+    <QrScanner />
+  </div>
 )
 
 export default App
